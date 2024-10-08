1b37728ba8ca2ffcb8c56449bc8fb7797256f140
Megha <124061733+meghaagrawal18@users.noreply.github.com>
2024-10-08 13:57:07 +0000

    Pending changes exported from your codespace

--- routes.py
+++ routes.py
@@ -1,19 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
-# two decorators, same function
+# Path to the text file
+TEXT_FILE = 'editable_text.txt'
+
+# Load text from file
+def load_text():
+    try:
+        with open(TEXT_FILE, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return ''
+
+# Save text to file
+def save_text(text):
+    with open(TEXT_FILE, 'w') as file:
+        file.write(text)
+
 @app.route('/')
-@app.route('/index.html')
+@app.route('/index.html') # get method
 def index():
-    return render_template('index.html', the_title='Tiger Home Page')
+    text = load_text()
+    return render_template('index.html', the_title='Text Editor', text=text)
 
-@app.route('/symbol.html')
-def symbol():
-    return render_template('symbol.html', the_title='Tiger As Symbol')
-
-@app.route('/myth.html')
-def myth():
-    return render_template('myth.html', the_title='Tiger in Myth and Legend')
+@app.route('/save', methods=['POST'])
+def save():
+    text = request.form['editor']
+    save_text(text)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
